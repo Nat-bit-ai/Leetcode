10e9c15e49b967c5083d4f50095789d6a89e6978
Natnael Zerihun <nathyzer21@gmail.com>
2026-03-01 03:27:15 -0800

    Time: 0 ms (100.00%) | Memory: 13 MB (80.73%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_to_index = {}  
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        return []
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2,7,11,15], 9))  
+    print(sol.twoSum([3,2,4], 6))      
+    print(sol.twoSum([3,3], 6))        
