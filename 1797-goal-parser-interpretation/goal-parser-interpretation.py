23c03f14982769d5342f5654bb207765e457fcf7
Natnael Zerihun <nathyzer21@gmail.com>
2026-03-25 15:44:13 +0300

    Time: 54 ms (5.32%) | Memory: 19.2 MB (92.98%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        while "()" in command:
+            command = command.replace("()" , "o")
+        while "(al)" in command:
+            command = command.replace("(al)" , "al")
+        return command
+
