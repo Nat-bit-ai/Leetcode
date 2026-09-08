298d2ac02abf7b25e608826abe89774c0d1ab7b4
Natnael Zerihun <nathyzer21@gmail.com>
2026-09-08 14:26:19 +0300

    Time: 0 ms (100%), Space: 19.5 MB (6.16%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,19 +1,49 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.head = None
+    def is_empty(self):
+        return self.head is None
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            rmv_node = self.head
+            self.head = self.head.next 
+            rmv_node.next = None
+            return rmv_node.val
+    def push(self, val):
+        new_node = Node(val)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            new_node.next = self.head 
+            self.head = new_node
+    def peak(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.head.val
 class Solution:
     def simplifyPath(self, path: str) -> str:
         s = path.split("/")
-        stack = []
-
+        stack = Stack()
         for i in s:
             if i == "":
                 continue
-
-            if i == ".":
+            elif i == ".":
                 continue
-
-            if i == "..":
-                if stack:
+            elif i == "..":
+                if not stack.is_empty():
                     stack.pop()
             else:
-                stack.append(i)
+                stack.push(i)
 
-        return "/" + "/".join(stack)
+        ans = []
+        while not stack.is_empty():
+            ans.append(stack.pop())
+        ans.reverse()
+        return "/" + "/".join(ans)
