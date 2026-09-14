2a6745d5f63f4c253ae1e041423bf9821d40a3d1
Natnael Zerihun <nathyzer21@gmail.com>
2026-09-14 12:34:40 +0300

    Time: 7 ms (98.28%), Space: 22.2 MB (53.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+
+        for str in strs:
+            key = ''.join(sorted(str))
+
+            if key not in map:
+                map[key] = []
+
+            map[key].append(str)
+
+        return list(map.values())
